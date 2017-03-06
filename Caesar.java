bc579128c40902a3cf9c90d1ce967bc448e26d21
Akshay Bahadur <akshay.dominator@gmail.com>
2017-03-06 20:53:58 +0530

    Caesar Cipher

---
+++ Caesar.java
@@ -0,0 +1,36 @@
+	import java.lang.*;
+	import java.io.*;
+	import java.util.*;
+	class Caesar
+	{
+		public static void main(String args[])throws IOException
+		{
+			Scanner scan = new Scanner(System.in);
+			String a=scan.nextLine();
+			String b=scan.nextLine();
+			int count=0,count1=1;
+			if(a.length()!=b.length())
+			{
+				System.out.println("-1");
+				return;
+			}
+			for(int i=0;i<a.length();i++)
+			{
+				char a1=a.charAt(i);
+				char b1=b.charAt(i);
+				count=Math.abs((int)a1-(int)b1);
+				//System.out.println(count);
+
+				if(i==0)
+					count1=count;
+				if(count1!=count&&count1!=(Math.abs(count-26)))
+				{
+				System.out.println("-1");
+				return;
+				}
+				
+			}
+				
+				System.out.println(count1);
+		}
+	}
